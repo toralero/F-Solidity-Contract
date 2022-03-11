0a4acfbbe6935d116d9436dd0befb9b7bcd37135
Rodrigo Reis <rodrigomiguelhoreis@gmail.com>
2022-03-12 03:39:56 +0800

    d

--- MyFirstSmartContract.sol
+++ MyFirstSmartContract.sol
@@ -50,7 +50,7 @@ contract BankDeposit {
   }
 
   //creating a new instance of a new struct
-  function createAccount(string _username, uint _ID, uint _accountbalance) external {
+  function createAccount(string  memory _username, uint memory _ID, uint memory _accountbalance) external {
     AccountHolder memory user1 = AccountHolder(msg.sender, _username, 1, 5);
   }
 
